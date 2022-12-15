460de897998fdafa6bf6818140ec8fd323b30609
Raul Jay Yanson <rjyansonx@gmail.com>
2022-12-15 09:15:49 +0800

    Initial commit

---
+++ Yanson_FuzzyLogic/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace Yanson_FuzzyLogic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(108, 366);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(735, 49);
+            this.button5.TabIndex = 21;
+            this.button5.Text = "One Step Calculation";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(434, 297);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(410, 49);
+            this.button4.TabIndex = 20;
+            this.button4.Text = "Calculate New Speed";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(583, 140);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(200, 49);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "Fuzzify";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(109, 140);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 49);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Fuzzify";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(206, 296);
+            this.textBox3.MaximumSize = new System.Drawing.Size(200, 200);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(200, 50);
+            this.textBox3.TabIndex = 17;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(103, 307);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 25);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Hours";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(109, 224);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(735, 49);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Defuzzify";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(422, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 25);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Fan Speed";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(583, 91);
+            this.textBox2.MaximumSize = new System.Drawing.Size(200, 200);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(200, 30);
+            this.textBox2.TabIndex = 13;
+            this.textBox2.Text = "2500";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(17, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 25);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Liter";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(109, 91);
+            this.textBox1.MaximumSize = new System.Drawing.Size(200, 200);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(200, 30);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.Text = "1";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(109, 22);
+            this.trackBar1.Maximum = 25;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(272, 56);
+            this.trackBar1.TabIndex = 22;
+            this.trackBar1.Value = 1;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(583, 22);
+            this.trackBar2.Maximum = 4500;
+            this.trackBar2.Minimum = 1;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(331, 56);
+            this.trackBar2.TabIndex = 23;
+            this.trackBar2.Value = 2500;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            this.trackBar2.ValueChanged += new System.EventHandler(this.trackBar2_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(922, 486);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+    }
+}
+
---
+++ Yanson_FuzzyLogic/Form1.cs
@@ -0,0 +1,168 @@
+﻿using DotFuzzy;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Yanson_FuzzyLogic
+{
+    public partial class Form1 : Form
+    {
+        FuzzyEngine fuzzyEngine;
+        MembershipFunctionCollection liter, speed, hour;
+        LinguisticVariable myliter, myspeed, myhour;
+        FuzzyRuleCollection myrules;
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetMembers();
+            SetRules();
+            SetFuzzyEngine();
+        }
+
+        public void SetMembers()
+        {
+
+            speed = new MembershipFunctionCollection();
+            speed.Add(new MembershipFunction("Slow", 1, 1, 1000, 1500));
+            speed.Add(new MembershipFunction("Medium", 1250, 1500, 2500, 3000));
+            speed.Add(new MembershipFunction("Fast", 2750, 3000, 4000, 4500));
+            myspeed = new LinguisticVariable("Speed", speed);
+
+            liter = new MembershipFunctionCollection();
+            liter.Add(new MembershipFunction("Low", 1, 1, 2, 4));
+            liter.Add(new MembershipFunction("Medium", 2, 4, 8, 10));
+            liter.Add(new MembershipFunction("High", 8, 12, 15, 24));
+            myliter = new LinguisticVariable("Liter", liter);
+
+            hour = new MembershipFunctionCollection();
+            hour.Add(new MembershipFunction("VeryShort", 1, 1, 4, 5));
+            hour.Add(new MembershipFunction("Short", 4, 5, 6, 7));
+            hour.Add(new MembershipFunction("Medium", 7, 7, 8, 9));
+            hour.Add(new MembershipFunction("Long", 8, 9, 10, 11));
+            hour.Add(new MembershipFunction("VeryLong", 10, 11, 12, 12));
+            myhour = new LinguisticVariable("Hour", hour);
+
+        }
+
+        public void SetRules()
+        {
+            myrules = new FuzzyRuleCollection();
+            myrules.Add(new FuzzyRule("IF (Speed IS Slow) AND (Liter IS Low) THEN Hour IS VeryLong"));
+            myrules.Add(new FuzzyRule("IF (Speed IS Slow) AND (Liter IS Medium) THEN Hour IS VeryLong"));
+            myrules.Add(new FuzzyRule("IF (Speed IS Slow) AND (Liter IS High) THEN Hour IS VeryLong"))
+                ;
+            myrules.Add(new FuzzyRule("IF (Speed IS Medium) AND (Liter IS Low) THEN Hour IS VeryShort"));
+            myrules.Add(new FuzzyRule("IF (Speed IS Medium) AND (Liter IS Medium) THEN Hour IS Medium"));
+            myrules.Add(new FuzzyRule("IF (Speed IS Medium) AND (Liter IS High) THEN Hour IS Long"));
+
+            myrules.Add(new FuzzyRule("IF (Speed IS Fast) AND (Liter IS Low) THEN Hour IS VeryShort"));
+            myrules.Add(new FuzzyRule("IF (Speed IS Fast) AND (Liter IS Medium) THEN Hour IS Short"));
+            myrules.Add(new FuzzyRule("IF (Speed IS Fast) AND (Liter IS High) THEN Hour IS Medium"));
+        }
+
+        public void SetFuzzyEngine()
+        {
+            fuzzyEngine = new FuzzyEngine();
+            fuzzyEngine.LinguisticVariableCollection.Add(myspeed);
+            fuzzyEngine.LinguisticVariableCollection.Add(myliter);
+            fuzzyEngine.LinguisticVariableCollection.Add(myhour);
+            fuzzyEngine.FuzzyRuleCollection = myrules;
+        }
+        private void button2_Click(object sender, System.EventArgs e)
+        {
+            myliter.InputValue = (Convert.ToDouble(textBox1.Text));
+            myliter.Fuzzify("Medium");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SetFuzzyEngine();
+            fuzzyEngine.Consequent = "Hour";
+            textBox3.Text = fuzzyEngine.Defuzzify() + "";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ComputeNewSpeed();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            FuziffyValues();
+            Defuzzy();
+            ComputeNewSpeed();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = trackBar1.Value.ToString();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar2_ValueChanged(object sender, EventArgs e)
+        {
+            textBox2.Text = trackBar2.Value.ToString();
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            myspeed.InputValue = (Convert.ToDouble(textBox2.Text));
+            myspeed.Fuzzify("Medium");
+        }
+
+        public void FuziffyValues()
+        {
+            myliter.InputValue = (Convert.ToDouble(textBox1.Text));
+            myliter.Fuzzify("Low");
+            myspeed.InputValue = (Convert.ToDouble(textBox2.Text));
+            myspeed.Fuzzify("Fast");
+        }
+
+        public void Defuzzy()
+        {
+            SetFuzzyEngine();
+            fuzzyEngine.Consequent = "Hour";
+            textBox3.Text = "" + fuzzyEngine.Defuzzify();
+        }
+        public void ComputeNewSpeed()
+        {
+
+            double oldSpeed = Convert.ToDouble(textBox2.Text);
+            double oldHour = Convert.ToDouble(textBox3.Text);
+            double oldLiter = Convert.ToDouble(textBox1.Text);
+            double newSpeed = ((1 - 0.1) * (oldSpeed)) + (oldHour - (0.1 * oldLiter));
+            textBox2.Text = "" + newSpeed;
+        }
+
+    }
+}
